002a6d8901bb32ffb1f990c47fe06a1afc9d8478
ayat93a <alkayedayat93@gmail.com>
2025-08-30 23:14:13 +0000

    ci cd

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Define a route to handle GET requests to the root URL
 app.get('/', (req, res) => {
-  res.send('Hello World from Rahma!');
+  res.send('Hello World from Devops!');
 });
 
 // Define the port number
